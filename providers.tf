b420dba6af1030e07f7f2a1728c4cdf0c19259a5
Olivier <olivier.hayot.dev@gmail.com>
2026-05-29 13:02:04 +0200

    Added remote backend for test purpose

--- providers.tf
+++ providers.tf
@@ -7,10 +7,10 @@ terraform {
     }
   }
   backend "azurerm" {
-    resource_group_name  = "<RESOURCE_GROUP_NAME>"
-    storage_account_name = "<STORAGE_ACCOUNT_NAME>"
-    container_name       = "<CONTAINER_NAME>"
-    key                  = "<KEY>"
+    resource_group_name  = "ohaymngmtproject"
+    storage_account_name = "ohaymngmtstorage"
+    container_name       = "tfstate"
+    key                  = "iac-azure-template.tfstate"
   }
 }
 
